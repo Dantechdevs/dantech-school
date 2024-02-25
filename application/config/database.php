07c57fe58a9aaae61aca4f769893461340ea1331
kelvinmurimi <murimikelvin229@gmail.com>
2024-02-24 16:52:22 -0800

    config database

--- application/config/database.php
+++ application/config/database.php
@@ -69,16 +69,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
+User “dantechd_master” was added to the database “dantechd_master”.
 */
 $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'localhost',
+	'username' => 'dantechd_master',
+	'password' => 'GreenZone@2023',
+	'database' => 'dantechd_master',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
